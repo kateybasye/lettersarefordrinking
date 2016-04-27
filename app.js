01a1c424dda0b23e4f8d16c0586e703cc2f8f058
Katey Basye <kbasye@gmail.com>
2016-04-27 19:17:48 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+		app.engine('pug', require('pug').__express);
+		app.set('view engine', 'pug');
+var path = require('path');
+		app.use(express.static(path.join(__dirname, 'public')));
+var mongo = require('mongodb').MongoClient;
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+// Routes
+var title = "Letters Are For Drinking";
+app.get('/', function (req, res) {
+	var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  res.render('index', {
+  	title: title,
+  	letters: letters,
+  	currentLetterIndex: 7
+  });
+});
+
+app.get('/bars/:letter', function (req, res) {
+	var letter = req.param('letter');
+	mongo.connect('mongodb://127.0.0.1:27017/l4d', function(err, db) {
+	  if (err) {
+	    throw err;
+	  }
+	  db.collection('bars').find({letter: letter}).toArray(function(err, result) {
+	    if (err) {
+	      throw err;
+	    }
+	    res.render('bars', {
+	    	letter: letter,
+	    	bars: result,
+	    	title: title + " // " + letter + " Bars"
+	    })
+	  }), {sort: 'city'};
+	});
+});
